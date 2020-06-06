e2020d0cad210f60d51baac67077e514a3fefa8c
jacob Dicken <jacobdicken2005@gmail.com>
2020-06-06 13:31:54 +0100

    Added Razor site files to the repo
    All necessary files from solution added to the repo

---
+++ RazorPagesMovie/Pages/Weather/Index.cshtml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages.Weather
+{
+    public class IndexModel : PageModel
+    {
+        // Location
+
+        public string CityName { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string CountryCode { get; set; }
+
+        // Main
+        public string Temp { get; set; }
+        public string FeelsLike { get; set; }
+        public string Humidity { get; set; }
+        public string WindSpeed { get; set; }
+        public string CloudCover { get; set; }
+
+        // Time
+        public string Timezone { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+
+        public void OnGet()
+        {
+            ClearVariables();
+        }
+
+        public void OnPost()
+        {
+            ClearVariables();
+            CityName = Request.Form["CityName"];
+            if (CityName == "")
+            {
+                return;
+            }
+            UpdateWeatherForm(CityName);
+        }
+
+        public void ClearVariables()
+        {
+            CityName = Lat = Lon = CountryCode = ""; //Clear Location variables
+            Temp = FeelsLike = Humidity = WindSpeed = CloudCover = ""; // Clear Main variables
+            Timezone = Sunrise = Sunset = ""; // Clear Time variables
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+        public string TimezoneToUTC(int Timezone)
+        {
+            int TimezoneHrs = Timezone / 3600;
+
+            if (TimezoneHrs == 0)
+            {
+                return "UTC + 0";
+            }
+            if (TimezoneHrs >= 1)
+            {
+                return "UTC +" + Convert.ToString(TimezoneHrs);
+            }
+            else
+            {
+                return "UTC " + Convert.ToString(TimezoneHrs);
+            }
+        }
+        public void UpdateWeatherForm(string CityName)
+        {
+            WeatherObject.RootObject weatherRoot = Weather.WeatherData(CityName); // deserialised json with weather data
+
+            // Location variables
+            Lat = Convert.ToString(weatherRoot.coord.Lat);
+            Lon = Convert.ToString(weatherRoot.coord.Lon);
+            CountryCode = weatherRoot.sys.Country;
+
+            // Main variables
+            Temp = Convert.ToString(weatherRoot.main.Temp) + "C";
+            FeelsLike = Convert.ToString(weatherRoot.main.Feels_like) + "C";
+            Humidity = Convert.ToString(weatherRoot.main.Humidity) + "%";
+            WindSpeed = Convert.ToString(weatherRoot.wind.Speed * 2.23694);
+            while (WindSpeed.Length > 5)
+            {
+                WindSpeed = WindSpeed.Remove(WindSpeed.Length - 1, 1);
+            }
+            WindSpeed = WindSpeed + "mph";
+            CloudCover = Convert.ToString(weatherRoot.clouds.All) + "%";
+
+            // Time variables
+            Timezone = TimezoneToUTC(weatherRoot.Timezone);
+            Sunrise = Convert.ToString(UnixTimeStampToDateTime(weatherRoot.sys.Sunrise));
+            Sunset = Convert.ToString(UnixTimeStampToDateTime(weatherRoot.sys.Sunset));
+        }
+    }
+}
---
+++ RazorPagesMovie/Pages/Weather/Weather.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace RazorPagesMovie.Pages.Weather
+{
+    public class Weather
+    {
+
+        private const string APIKey = "66193b691cab0a63f69bb9930e142587"; // KEEP THIS SECRET
+        const string BASEURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+        public static WeatherObject.RootObject WeatherData(string location)
+        {
+
+            location = whitespaceRemover(location);
+            string url = BASEURL + location + "&mode=json&units=metric&appid=" + APIKey; // builds the URL from user input data
+
+            // creates a web client instance and downloads the json data from provided URL to the
+            // variable 'json'
+            WebClient wc = new WebClient(); // creates a new instance of the WebClient class
+
+            var json = wc.DownloadString(url);
+            //TODO HANDLE System.Net.WebException
+
+            var root = JsonConvert.DeserializeObject<WeatherObject.RootObject>(json); // converts the json data into an object made of classes
+            return root;
+
+
+        }
+
+
+        /// <summary>
+        /// method to remove all whitespace in a string for compatibility with URLs
+        /// </summary>
+        /// <param name="s">the input string to be cleared of whitespace </param>
+        /// <returns> the input string is returned after having whitespace removed</returns>
+        static string whitespaceRemover(string s)
+        {
+            s.Replace(" ", "");
+            return s;
+        }
+
+
+    }
+    /// <summary>
+    /// Nested classes to deserialise weather data JSON into
+    /// </summary>
+    public class WeatherObject
+    {
+        public class Coord
+        {
+            public double Lon { get; set; }
+            public double Lat { get; set; }
+        }
+
+        public class Weather
+        {
+            public int ID { get; set; }
+            public string Main { get; set; }
+            public string Description { get; set; }
+            public string Icon { get; set; }
+        }
+
+        public class Main
+        {
+            public double Temp { get; set; }
+            public double Feels_like { get; set; }
+            public double Temp_min { get; set; }
+            public double Temp_max { get; set; }
+            public int Pressure { get; set; }
+            public int Humidity { get; set; }
+        }
+
+        public class Wind
+        {
+            public double Speed { get; set; }
+            public int Deg { get; set; }
+        }
+
+        public class Clouds
+        {
+            public int All { get; set; }
+        }
+
+        public class Sys
+        {
+            public int Type { get; set; }
+            public int ID { get; set; }
+            public string Country { get; set; }
+            public int Sunrise { get; set; }
+            public int Sunset { get; set; }
+        }
+
+        public class Misc
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int Cod { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Coord coord { get; set; }
+            public List<Weather> weather { get; set; }
+            public string @base { get; set; }
+            public Main main { get; set; }
+            public int visibility { get; set; }
+            public Wind wind { get; set; }
+            public Clouds clouds { get; set; }
+            public int dt { get; set; }
+            public Sys sys { get; set; }
+            public Misc misc { get; set; }
+            public int Timezone { get; set; }
+        }
+    }
+}
